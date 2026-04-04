56c745a0f57f91978fe8c8837557f78125ac9395
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-04-04 15:37:59 +0300

    feat: create mini ATM project

---
+++ Projects/Mini_ATM/atm.py
@@ -0,0 +1,42 @@
+attempt_pin = 0
+import sys
+
+print('===== Welcome to the ATM =====')
+
+while attempt_pin < 3:
+    pin = input('Please enter a pin: ')
+    if pin == '1234':
+        print('Access granted')
+        break
+    else:
+        print('Pin not recognised')
+        attempt_pin = attempt_pin + 1
+
+if attempt_pin == 3:
+    print('Access denied ')
+    sys.exit()
+
+
+balance = 1000
+
+while True:
+    print('\nChoose an option:\n1 - Check balance\n2 - Deposit\n3 - Withdraw\n4 - Exit')
+    choice = input('Enter your choice: ')
+    if choice == '1':
+        print('Your balance: ' + str(balance))
+    elif choice == '2':
+        am_depos = int(input('Enter the amount to deposit: '))
+        balance += am_depos
+        print('Deposit successful. Your new balance is: ' + str(balance))
+    elif choice == '3':
+        withdraw = int(input('Enter the withdrawal amount: '))
+        if balance < withdraw:
+            print('Transaction failed: not enough balance.')
+            continue
+        balance -= withdraw
+        print('Withdrawal successful. Your remaining balance is: ' + str(balance))
+    elif choice == '4':
+        print ('=== Thank you. Goodbye. ===')
+        break
+    else:
+        print('Please enter a valid option')
