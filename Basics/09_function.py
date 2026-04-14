279bae9f32741a4eff7d88f5841b220eddec4c76
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-04-14 12:36:38 +0300

    chore: add function practice examples

---
+++ Basics/09_function.py
@@ -0,0 +1,20 @@
+# Task 1: Functions for calculating perimeter and area
+
+def calculate_p(x, y):
+    print('-' * 10)
+    print(f'Perimeter; {2 * (x + y)}')
+    print(f'Area: {x * y}')
+
+calculate_p(10, 5)
+calculate_p(15, 7)
+calculate_p(11, 8)
+
+# Task 2: Functions of multiplying numbers
+print('~' * 20)
+
+def multiply(x = 1, y = 1):
+    print(x * y)
+
+multiply(10, 5)
+multiply(15)
+multiply()
