f484e8517db575484380f49dec7b0c7e71384ac3
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-04-17 16:26:18 +0300

    style: remove extra empty lines

--- Basics/10_closures.py
+++ Basics/10_closures.py
@@ -3,7 +3,6 @@
 def say_name(name):
     def say_hello():
         print(f'Hello {name}!')
-
     return say_hello
 
 f = say_name('Bohdan')
@@ -14,7 +13,6 @@ f()
 def strip_spring (strip_chars = ' '):
     def do_strip (striping):
         return striping.strip(strip_chars)
-
     return do_strip
 
 strip1 = strip_spring()
