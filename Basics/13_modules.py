e0b5b06ff1a32af65b404ae90fca87523e896a97
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-04-22 12:46:40 +0300

    feat: add modules practice examples

---
+++ Basics/13_modules.py
@@ -0,0 +1,27 @@
+# Task 1
+
+import sys
+import os
+import platform
+from math import ceil, sqrt as s
+import time
+import datetime as dt
+import own_module
+
+print('Sys:', sys.path)
+print('Os:', os.name)
+print('Platform:', platform.system())
+
+print('Math:', ceil(s(121)))
+
+time.sleep(1)
+print('Datetime:', dt.datetime.now())
+
+# Task 2
+
+print(own_module.name)
+
+own_module.say_hi()
+
+res = own_module.add_3_numbers(1, 1, 1)
+print(res)
---
+++ Basics/own_module.py
@@ -0,0 +1,9 @@
+name = 'Bohdan'
+
+
+def say_hi():
+    print('Hello')
+
+
+def add_3_numbers(a,b,c):
+    return a + b + c
