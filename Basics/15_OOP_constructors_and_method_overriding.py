2771883f7a21daa2dcbb2ef904c470deade331a2
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-04-23 18:13:58 +0300

    feat: add OOP constructors and method overriding practice examples

---
+++ Basics/15_OOP_constructors_and_method_overriding.py
@@ -0,0 +1,19 @@
+class Robot:
+    name = None
+    age = None
+    combat = None
+
+    def __init__(self, name='C-3PO', age=10, combat=False):
+        self.set_data(name, age, combat)
+        self.get_data()
+
+    def set_data(self, name, age, combat):
+        self.name = name
+        self.age = age
+        self.combat = combat
+
+    def get_data(self):
+        print('Name:', self.name, 'Age:', self.age, 'Combat:', self.combat)
+
+robot1 = Robot(age=20)
+robot2 = Robot('C-3PO', 10, False)
