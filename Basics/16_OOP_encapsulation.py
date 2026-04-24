36bc3c4edd3a60b4c21745522521ad71c80982ee
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-04-24 13:31:22 +0300

    feat: add OOP encapsulation practice examples

---
+++ Basics/16_OOP_encapsulation.py
@@ -0,0 +1,18 @@
+class MyClass:
+
+    def __init__(self):
+        self.__private = 10
+        self.public = 20
+
+    def some_method(self):
+        return self.__private + self.public
+
+    def __private_method(self):
+        print('This is private method')
+
+a = MyClass()
+
+print(a.public)
+# print(a.__private)
+# a.__private_method()
+print(a.some_method())
