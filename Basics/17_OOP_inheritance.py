9763e167972e37d9096f6103828a3888c0022093
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-04-25 13:31:08 +0300

    feat: add OOP inheritance practice examples

---
+++ Basics/17_OOP_inheritance.py
@@ -0,0 +1,26 @@
+class Bild:
+
+    def __init__(self, years, city):
+        self.years = years
+        self.city = city
+
+        self.get_info()
+
+    def get_info(self):
+        print('Year:', self.years, 'City:', self.city)
+
+class School(Bild):
+
+    def __init__(self, years, city, pupils):
+        super(School, self).__init__(years, city)
+        self.pupils = pupils
+
+class House(Bild):
+    pass
+
+class Shop(Bild):
+    pass
+
+school = School(1973, 'Dnipro', 500)
+house = Bild(1985, 'Kyiv')
+shop = Bild(1999, 'Odesa')
