2565cb0c8f1293742f33a86d22ccc75ab9366e8f
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-04-28 16:21:15 +0300

    feat: add abstraction practice examples

---
+++ Basics/19_OOP_abstraction.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class Payment(ABC):
+    @abstractmethod
+    def pay(self, amount):
+        pass
+
+class CardPayment(Payment):
+    def pay(self, amount):
+        print(f'Paid {amount} by card')
+
+class PayPalPayment(Payment):
+    def pay(self, amount):
+        print(f'Paid {amount} via PayPal')
+
+class CryptoPayment(Payment):
+    def pay(self, amount):
+        print(f'Paid {amount} in crypto')
+
+def process_payment(payment: Payment):
+    payment.pay(100)
+
+process_payment(CardPayment())
+process_payment(PayPalPayment())
+process_payment(CryptoPayment())
