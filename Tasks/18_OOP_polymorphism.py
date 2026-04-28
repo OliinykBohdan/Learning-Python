a93009948fa383593ec8b36ee42b7dbf034be1ce
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-04-28 13:28:11 +0300

    feat: add tasks 6, 7, 8 and 9

--- Tasks/18_OOP_polymorphism.py
+++ Tasks/18_OOP_polymorphism.py
@@ -199,3 +199,184 @@ bird = Bird()
 
 movement(car)
 movement(bird)
+
+# Task 6: Override + super()
+#
+# Description:
+#
+# Extend parent behavior
+#
+# Task:
+#
+# Base class Employee:
+#
+# method work() → 'Working'
+#
+# Child class Developer:
+#
+# override work()
+# call parent method
+# add: 'Writing code'
+
+print('-' * 10, 'Task 6:', sep = '\n')
+
+class Employee:
+    def work(self):
+        print('Working')
+
+class Developer(Employee):
+    def work(self):
+        super().work()
+        print('Writing code')
+
+person = Developer()
+
+person.work()
+
+# Task 7: Real-life Polymorphism
+#
+# Description:
+#
+# Payment system
+#
+# Task:
+#
+# Create base class Payment:
+#
+# method pay()
+#
+# Create:
+#
+# CardPayment → 'Paid with card'
+# CashPayment → 'Paid with cash'
+#
+# Function:
+#
+# def process_payment(payment):
+#     payment.pay()
+
+print('-' * 10, 'Task 7:', sep = '\n')
+
+class Payment:
+    def pay(self):
+        pass
+
+class CardPayment(Payment):
+    def pay(self):
+        print('Paid with card')
+
+class CashPayment(Payment):
+    def pay(self):
+        print('Paid with cash')
+
+def process_payment(payment):
+    payment.pay()
+
+process_payment(CardPayment())
+process_payment(CashPayment())
+
+# Task 8: Return Values
+#
+# Description:
+#
+# Polymorphism with return
+#
+# Task:
+#
+# Create:
+#
+# Dog → speak() returns 'Woof'
+# Cat → returns 'Meow'
+#
+# Store in list
+#
+# Return all sounds as list:
+#
+# ['Woof', 'Meow']
+
+print('-' * 10, 'Task 8:', sep = '\n')
+
+class Animal:
+    def speak(self):
+        return 'Some sound'
+
+class Dog(Animal):
+    def speak(self):
+        return 'Woof'
+
+class Cat(Animal):
+    def speak(self):
+        return 'Meow'
+
+animals = [Dog(), Cat()]
+
+sounds = [animal.speak() for animal in animals]
+
+print(sounds)
+
+# Task 9: Mini Challenge
+#
+# Description:
+#
+# Mix everything
+#
+# Task:
+#
+# Create:
+#
+# class Animal
+# subclasses: Dog, Cat, Bird
+#
+# Each has:
+#
+# speak()
+# move()
+#
+# Dog → run
+# Bird → fly
+# Cat → walk
+#
+# Loop:
+#
+# animals = [...]
+#
+# Print:
+#
+# Woof - run
+# Meow - walk
+# Tweet - fly
+
+print('-' * 10, 'Task 9:', sep = '\n')
+
+class Animal:
+    def speak(self):
+        return 'Some sound'
+
+    def move(self):
+        return 'Some move'
+
+class Dog(Animal):
+    def speak(self):
+        return 'Woof'
+
+    def move(self):
+        return 'run'
+
+class Cat(Animal):
+    def speak(self):
+        return 'Meow'
+
+    def move(self):
+        return 'walk'
+
+class Bird(Animal):
+    def speak(self):
+        return 'Tweet'
+
+    def move(self):
+        return 'fly'
+
+animals = [Dog(), Cat(), Bird()]
+
+for animal in animals:
+    print(f'{animal.speak()} - {animal.move()}')
