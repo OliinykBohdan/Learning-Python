eb60a4316fcfe4c21b2fb79f18f02b8ec4ad46f9
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-05-15 16:56:10 +0300

    refactor: improve interaction with task list

--- Mini_Projects/02_Todo_App/Todo_App.py
+++ Mini_Projects/02_Todo_App/Todo_App.py
@@ -7,28 +7,44 @@ class TaskManager:
     def __init__(self):
         self.tasks = []
 
-    def add_task(self, description):
-        self.tasks.append(Task(description))
+    def add_task(self, task):
+        print(f'Task "{task.description}" added.')
+        self.tasks.append(task)
 
-    def complete_task(self, index):
-        self.tasks[index].done = True
+    def complete_task(self, task):
+        if task not in self.tasks:
+            print(f'Task "{task.description}" not found.')
+            return
 
-    def remove_task(self, index):
-        self.tasks.pop(index)
+        task.done = True
+        print(f'\n~~~ Task "{task.description}" completed ~~~')
+
+    def remove_task(self, task):
+        if task not in self.tasks:
+            print(f'Task "{task.description}" not found.')
+            return
+
+        self.tasks.remove(task)
+        print(f'Task "{task.description}" removed.')
 
     def show_tasks(self):
-        print('\n===== All tasks =====\n')
+        print('\n===== All tasks =====')
+
         for task in self.tasks:
-            print(f'Description: {task.description}\n Status: {task.done}\n')
+            status = 'Completed' if task.done else 'In progress'
+            print(f'Description: {task.description}\n Status: {status}\n')
 
 task_manager = TaskManager()
 
-task_manager.add_task('Defeat the OOP')
-task_manager.add_task('Learn classes and objects')
-task_manager.add_task('Learn the principles of OOP')
+task1 = Task('Defeat the OOP')
+task2 = Task('Learn classes and objects')
+task3 = Task('Learn the principles of OOP')
 
-task_manager.complete_task(0)
-task_manager.complete_task(1)
-task_manager.complete_task(2)
+task_manager.add_task(task1)
+task_manager.add_task(task2)
+task_manager.add_task(task3)
+
+task_manager.complete_task(task2)
+task_manager.complete_task(task3)
 
 task_manager.show_tasks()
